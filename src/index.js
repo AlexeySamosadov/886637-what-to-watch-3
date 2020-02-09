7d244296fd6a142861e1bfa9d4fd5b0685ecd2ab
Aleexxeyy <asamosadov@gmail.com>
2020-02-09 03:22:38 +0300

    сделал задание

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-const bodyElement = document.querySelector(`body`);
-bodyElement.textContent = `Hello, hello, hello!`;
+import React from "react";
+import ReactDOM from "react-dom";
+
+const MovieInfo = {
+  name: `The Grand Budapest Hotel`,
+  date: 2014,
+  genre: `Drama`,
+};
+
+import App from "./components/app/app.jsx";
+ReactDOM.render(
+    <App
+      movieInfo={MovieInfo}
+    />,
+    document.querySelector(`.app`)
+);
