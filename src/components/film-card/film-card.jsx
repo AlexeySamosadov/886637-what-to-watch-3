408d67163145363611365d09cc392b59ea9bfb24
AlexeySamosadov <asamosadov@gmail.com>
2020-03-28 21:52:23 +0300

    Сделал FilmCard функциональным

--- src/components/film-card/film-card.jsx
+++ src/components/film-card/film-card.jsx
@@ -5,28 +5,23 @@ import withVideoPlayer from "../../hocs/with-video-player.js";
 
 const VideoPlayerWrapper = withVideoPlayer(VideoPlayer);
 
-class FilmCard extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
 
-  render() {
-    const {filmData, onTitleClick} = this.props;
-    const {src, name, id, srcVideo} = filmData;
-    return (
-      <article onClick={
-        ()=> onTitleClick(id)
-      }
-      className="small-movie-card catalog__movies-card"
-      >
-        <VideoPlayerWrapper src={src} srcVideo={srcVideo} width="280" height="175"/>
-        <h3 className="small-movie-card__title">
-          <a className="small-movie-card__link" href="movie-page.html">{name}</a>
-        </h3>
-      </article>
-    );
-  }
-}
+const FilmCard = ({filmData, onTitleClick})=> {
+  const {src, name, id, srcVideo} = filmData;
+  return (
+    <article onClick={
+      ()=> onTitleClick(id)
+    }
+    className="small-movie-card catalog__movies-card"
+    >
+      <VideoPlayerWrapper src={src} srcVideo={srcVideo} width="280" height="175"/>
+      <h3 className="small-movie-card__title">
+        <a className="small-movie-card__link" href="movie-page.html">{name}</a>
+      </h3>
+    </article>
+  );
+};
+
 
 FilmCard.propTypes = {
   onTitleClick: PropTypes.func.isRequired,
