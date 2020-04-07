1665b7332e71e03080ca08325e707198d42c4d02
AlexeySamosadov <asamosadov@gmail.com>
2020-04-07 21:23:02 +0300

    Исправил ошибки

--- src/components/const/const.js
+++ src/components/const/const.js
@@ -1,3 +1,22 @@
 export const genreType = {
   ALL: `All genres`,
 };
+
+export const playerType = {
+  MOVIE: `MOVIE`,
+  TRAILER: `TRAILER`,
+};
+
+export const playerClass = {
+  PLAYER_VIDEO: `player__video`,
+};
+
+export const keyCode = {
+  ESCAPE: `Escape`,
+  SPACE: `Space`,
+};
+
+export const typeEvent = {
+  FULL_SCREEN_CHANGE: `fullscreenchange`,
+  KEYDOWN: `keydown`,
+};
